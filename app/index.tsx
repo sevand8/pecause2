1bcb8b8ef5ebe9e5059101d0b6c4d8ac8314fea4
seb.and156 <seb.and156@gmail.com>
2025-10-01 17:05:28 +0000

    💄ui(ui/CustomText): Hacer por defecto dark en ´false´ - dark se vuelve opcional y por defecto en false

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,14 @@
 import CustomText from "@/components/ui/CustomText";
 import "@/global.css";
-import {View} from "react-native";
+import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-        <View className="bg-[#026A75] w-screen h-screen">
+        <View className="bg-[#E2B878] w-screen h-screen">
         <CustomText variant="large" dark={false}>
           Hello World
         </CustomText>
       </View>
-    </View>
+
   );
 }
